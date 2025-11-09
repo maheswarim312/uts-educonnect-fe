33ad7df0a79e85e44d32f4fb9e8824dad5c6acdd
mahesmahfud <mahesmahfud@gmail.com>
2025-11-09 14:56:46 +0700

    api-user fix

--- app/context/AuthContext.js
+++ app/context/AuthContext.js
@@ -29,7 +29,7 @@ export function AuthProvider({ children }) {
 
     try {
       const res = await fetch(
-        "https://api.uts-educonnect.online/api/v1/auth/login",
+        "https://api-user.uts-educonnect.online/api/v1/auth/login",
         {
           method: "POST",
           headers: {
@@ -68,7 +68,7 @@ export function AuthProvider({ children }) {
 
     try {
       const res = await fetch(
-        "https://api.uts-educonnect.online/api/v1/auth/register",
+        "https://api-user.uts-educonnect.online/api/v1/auth/register",
         {
           method: "POST",
           headers: {
@@ -99,19 +99,14 @@ export function AuthProvider({ children }) {
   };
 
   const apiFetch = async (endpoint, options = {}) => {
-    // 1. Ambil token dari "memori" brankas
-    // Kita pakai 'useState' (token) di sini biar 'realtime',
-    // tapi 'localStorage' juga bisa jadi cadangan
     const storedToken = token || localStorage.getItem("educonnect_token");
 
-    // 2. Siapkan header default
     const defaultHeaders = {
       "Content-Type": "application/json",
       Accept: "application/json",
-      Authorization: `Bearer ${storedToken}`, // <-- INI KUNCINYA
+      Authorization: `Bearer ${storedToken}`, 
     };
 
-    // 3. Gabungkan header default dengan header tambahan (jika ada)
     const config = {
       ...options,
       headers: {
@@ -120,35 +115,27 @@ export function AuthProvider({ children }) {
       },
     };
 
-    // 4. Panggil "Terminal" kita
     const res = await fetch(
-      `https://api.uts-educonnect.online${endpoint}`,
+      `https://api-user.uts-educonnect.online${endpoint}`,
       config
     );
 
-    // 5. "Satpam" Token (Jaga-jaga kalau token expired)
     if (res.status === 401) {
-      // Token tidak valid!
-      logout(); // Paksa logout
+      logout();
       throw new Error("Token tidak valid, silakan login ulang");
     }
 
     if (!res.ok) {
-      // Tangani error API lainnya
       const errorData = await res.json();
 
-      // INI LOGIKA BARU UNTUK TANGKAP ERROR 422 (VALIDASI)
-      // (Kita comot dari fungsi 'register' kita yang udah pinter)
       if (res.status === 422 && errorData.errors) {
         const errorMessages = Object.values(errorData.errors).flat().join(", ");
         throw new Error(errorMessages); // Cth: "The password must be at least 8 characters."
       }
 
-      // Ini untuk error lain (404, 500, dll)
       throw new Error(errorData.message || "Terjadi kesalahan API");
     }
 
-    // 6. Kalau sukses, kembalikan datanya
     return res.json();
   };
 
